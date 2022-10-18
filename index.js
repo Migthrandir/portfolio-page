d4d727f38215cd69af1791342f808823c75f9f80
Miguel Araya <maaharaya@gmail.com>
2022-10-18 14:01:59 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,37 @@
+function createProjectCard (project) {
+
+    project.forEach(project => {
+        const container = document.getElementById('project-cards-container');
+        
+        const projectCard = document.createElement('div');
+        projectCard.classList.add('project-card');
+        container.appendChild(projectCard);
+
+        const projectTitle = document.createElement('h3');
+        projectCard.appendChild(projectTitle);
+        projectTitle.innerText = project.title;
+
+        const projectImg = document.createElement('img');
+        projectCard.appendChild(projectImg);
+        projectImg.setAttribute('alt', "imagen del proyecto");
+        projectImg.setAttribute('src', project.img);
+        
+        const projectFoot = document.createElement('div');
+        projectCard.appendChild(projectFoot);
+        projectFoot.classList.add('project-card--foot');
+        
+        const buttonView = document.createElement('button');
+        projectFoot.appendChild(buttonView);
+        buttonView.setAttribute('onclick', `window.open('${project.buttons.view}', '_blank')`);
+        buttonView.innerText = 'View';
+        
+        const buttonGithub = document.createElement('button');
+        projectFoot.appendChild(buttonGithub);
+        buttonGithub.setAttribute('onclick', `window.open('${project.buttons.github}', '_blank')`);
+        buttonGithub.innerText = 'GitHub-Repo';
+        
+
+    });
+}
+
+createProjectCard(project);
---
+++ projects.js
@@ -0,0 +1,10 @@
+var project = [
+    {
+        title: 'Batata bit',
+        img: './assets/batata_bit.png',
+        buttons: {
+            view: 'https://migthrandir.github.io/Curso-Mobile-First/',
+            github: 'https://github.com/Migthrandir/Curso-Mobile-First',
+        }      
+    },
+];
